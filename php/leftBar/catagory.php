842200180d6d3e5cdf631d2058d85731c1028d6a
deepak6sp <deepak6sp@gmail.com>
2014-11-28 01:54:18 +1100

    added solar.php

--- php/leftBar/catagory.php
+++ php/leftBar/catagory.php
@@ -7,16 +7,15 @@
         <nav class="catagoryList">
           <ul>
             <li><a href="<?php echo "$websiteurl/toys.php" ?>">Toys</a></li>
-            <li><a href="<?php echo "$websiteurl/toys.php" ?>">Canadian Solor</a></li>
+            <li><a href="<?php echo "$websiteurl/solar.php" ?>">Canadian Solor</a></li>
             <li><a href="<?php echo "$websiteurl/toys.php" ?>">Lightway</a></li>
-            <li><a href="<?php echo "$websiteurl/toys.php" ?>">Yingli Solar</a></li>
+            <li><a href="<?php echo "$websiteurl/solar.php" ?>">Yingli Solar</a></li>
             <li><a href="<?php echo "$websiteurl/toys.php" ?>">Renesola</a></li>
-            <li><a href="<?php echo "$websiteurl/toys.php" ?>">Jinko</a></li>
+            <li><a href="<?php echo "$websiteurl/solar.php" ?>">Jinko</a></li>
             <li><a href="<?php echo "$websiteurl/toys.php" ?>">LG</a></li>
-            <li><a href="<?php echo "$websiteurl/toys.php" ?>">SolarWorld</a></li>
+            <li><a href="<?php echo "$websiteurl/solar.php" ?>">SolarWorld</a></li>
             <li><a href="<?php echo "$websiteurl/toys.php" ?>">ET Solar</a></li>
-            <li><a href="<?php echo "$websiteurl/toys.php" ?>">Solis</a></li>
-            <li><a href="<?php echo "$websiteurl/toys.php" ?>">SolaX</a></li>
+            <li><a href="<?php echo "$websiteurl/solar.php" ?>">Solis</a></li>
           </ul>
         </nav>
   </div>
---
+++ solar.php
@@ -0,0 +1,21 @@
+<?php include_once("php/header.php"); ?>
+<!--menubar-->
+		<header class="bottomHeader">
+    	<?php include_once("php/navHeader.php"); ?>
+    </header>
+<!---left Bar-->
+		<aside class="leftBar">
+				<?php include("php/leftBar/catagory.php"); ?>
+    </aside>
+<!---main content-->
+	<main id="mainContent">
+		<div class="imageWrapper"><img src="images/solar1.jpg"><h6>Price $1500<br>Stock Available - <input placeholder="50"></h6></img></div>
+		<div class="imageWrapper"><img src="images/solar2.jpg"><h6>Price $1000<br>Stock Available - <input placeholder="15"></h6></img></div>
+		<div class="imageWrapper"><img src="images/solar3.jpg"><h6>Price $1200<br>Stock Available - <input placeholder="30"></h6></img></div>
+		<div class="imageWrapper"><img src="images/solar4.jpg"><h6>Price $1800<br>Stock Available - <input placeholder="60"></h6></img></div>
+		<div class="imageWrapper"><img src="images/solar5.jpg"><h6>Price $2500<br>Stock Available - <input placeholder="10"></h6></img></div>
+		<div class="imageWrapper"><img src="images/solar6.jpg"><h6>Price $1000<br>Stock Available - <input placeholder="50"></h6></img></div>
+   	</main>	
+
+<!--footer-->
+   <?php include_once("php/footer.php"); ?> 
